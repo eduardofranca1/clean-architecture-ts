8e46397e96e167b3b10387924a8628b00ea63e76
eduardofranca1 <francaedu98@gmail.com>
2025-04-07 12:30:38 -0300

    chore: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  globalIgnores(["dist/**"]),
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-var-requires": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+];
--- src/main/factories/controller/create-user.controller.ts
+++ src/main/factories/controller/create-user.controller.ts
@@ -1,12 +1,10 @@
-import { ICreateUserRepository } from "../../../application/ports/repositories/create-user-repository";
 import { CreateUserUseCase } from "../../../application/use-cases/create-user-use-case";
 import { CreateUserMongoRepository } from "../../../infrastructure/repositories/create-user-mongo.repository";
 import { CreateUserContoller } from "../../../presentation/controllers/create-user.controller";
 import { GenericCreatedResponse } from "../../../presentation/responses/generic-created-response";
 
 export const createUserControllerFactory = () => {
-  const createUserRepository: ICreateUserRepository =
-    new CreateUserMongoRepository();
+  const createUserRepository = new CreateUserMongoRepository();
 
   const createUserUseCase = new CreateUserUseCase(createUserRepository);
 

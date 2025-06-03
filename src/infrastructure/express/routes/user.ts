6ea90413334cbf4c158364bdc5f228f0576858d0
eduardofranca1 <francaedu98@gmail.com>
2025-06-03 10:43:19 -0300

    feat: add update user router

--- src/infrastructure/express/routes/user.ts
+++ src/infrastructure/express/routes/user.ts
@@ -4,6 +4,7 @@ import { createUserControllerFactory } from '@/main/factories/controller/create-
 import { findAllUsersControllerFactory } from '@/main/factories/controller/find-all-users.controller';
 import { findUserByIdControllerFactory } from '@/main/factories/controller/find-user-by-id.controller';
 import { deleteUserByIdControllerFactory } from '@/main/factories/controller/delete-user.controller';
+import { updateUserControllerFactory } from '@/main/factories/controller/update-user.controller';
 
 export const userRoutes = Router();
 
@@ -11,8 +12,10 @@ const { createUserController } = createUserControllerFactory();
 const { findAllUsersController } = findAllUsersControllerFactory();
 const { findUserByIdController } = findUserByIdControllerFactory();
 const { deleteUserByIdController } = deleteUserByIdControllerFactory();
+const { updateUserController } = updateUserControllerFactory();
 
 userRoutes.post('/', expressRouteAdapter(createUserController));
 userRoutes.get('/', expressRouteAdapter(findAllUsersController));
 userRoutes.get('/:id', expressRouteAdapter(findUserByIdController));
+userRoutes.put('/:id', expressRouteAdapter(updateUserController));
 userRoutes.delete('/:id', expressRouteAdapter(deleteUserByIdController));
